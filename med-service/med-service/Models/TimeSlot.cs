f18917a02ab96a3e81e3a466c2b2b22ad71549a7
Anne738 <annbulgakova10@gmail.com>
2025-02-03 16:53:53 +0200

    add models of schedule and timeslot

---
+++ med-service/med-service/Models/TimeSlot.cs
@@ -0,0 +1,19 @@
+﻿using static med_service.Models.Scgedule;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace med_service.Models
+{
+    public class TimeSlot
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Schedule")]
+        public int ScheduleId { get; set; }
+        public Schedule Schedule { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
