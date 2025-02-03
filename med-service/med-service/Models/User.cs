ec4683ea5919eee7b86b0228e4d99400edbf0b1d
Daryna Boruk <darynaboruk@gmail.com>
2025-02-03 15:48:21 +0200

    created skeleton project + models

---
+++ med-service/med-service/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace med_service.Models
+{
+    public class User
+    {
+        public enum UserRole
+        {
+            Admin,
+            Doctor,
+            Patient
+        }
+        public UserRole Role { get; set; }
+
+        [Key]
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+    }
+}
