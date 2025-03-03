c70ecc2a6203e7bae4af857b2459ccdb226718e5
Anton Babych <babichanton4@gmail.com>
2025-03-03 02:22:29 +0200

    fixed hospital + patient contollers; create successful; minor changed models

--- med-service/med-service/Models/Patient.cs
+++ med-service/med-service/Models/Patient.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 namespace med_service.Models
 {
     public class Patient
     {
+        public Patient()
+        {
+            Appointments = new List<Appointment>();
+        }
         public int Id { get; set; }
         public string UserId { get; set; } // IdentityUser.Id
+
+        [ValidateNever]
         public User User { get; set; }
 
         public DateTime DateOfBirth { get; set; }
