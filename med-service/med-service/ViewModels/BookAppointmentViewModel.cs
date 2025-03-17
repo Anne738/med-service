8c78b6da9e558b5319bb4c9d8d9900439a6301e4
Daryna Boruk <darynaboruk@gmail.com>
2025-03-17 14:10:08 +0200

    Created ViewModel for User

---
+++ med-service/med-service/ViewModels/BookAppointmentViewModel.cs
@@ -0,0 +1,19 @@
+﻿// Файл: Models/ViewModels/BookAppointmentViewModel.cs
+using med_service.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace med_service.ViewModels
+{
+    public class BookAppointmentViewModel
+    {
+        public int TimeSlotId { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public Schedule.DayOfWeek ScheduleDay { get; set; }
+        public int PatientId { get; set; }
+        public string Notes { get; set; }
+        public List<SelectListItem> PatientsList { get; set; } = new List<SelectListItem>();
+    }
+}
