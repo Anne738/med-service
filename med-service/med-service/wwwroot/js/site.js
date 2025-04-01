77d0c0bcedda6c3ee18174c03af87dc9ec9d5a15
liljekki <123546wwe123546wwe@gmail.com>
2025-04-01 21:28:15 +0300

    partialview

--- med-service/med-service/wwwroot/js/site.js
+++ med-service/med-service/wwwroot/js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code.
 function addEntity(url) {
     $.ajax({
-        url: url,
+        url: "/" + url + "/Create",
         type: "GET",
         processData: false,
         contentType: false,
@@ -20,5 +20,124 @@ function addEntity(url) {
         $('#modalX').empty();
         $('#modalX').append(data);
         $('.modal').modal('show');
+    });
+}
+
+//function addEntity(url) {
+//    $.ajax({
+//        url: "/" + url + "/Create",
+//        type: "GET",
+//        processData: false,
+//        contentType: false,
+//        data: null,
+//        async: true,
+//        error: function (jqxhr, textStatus, errorThrown) {
+//            console.error('Error', jqxhr);
+//            $('.modal').remove();
+//            $('.modal-backdrop').remove();
+//            bootbox.alert(jqxhr.responseText);
+//        }
+//    }).done((data) => {
+//        // Log the received data to see what's being returned
+//        console.log("Response data:", data);
+
+//        // Make sure the modal element exists
+//        if (!$('#modalX').length) {
+//            $('body').append('<div class="modal fade" id="modalX" tabindex="-1" aria-hidden="true"></div>');
+//        }
+
+//        // Clear and append the new content
+//        $('#modalX').empty();
+//        $('#modalX').html(data);
+
+//        // Initialize and show the modal after content is loaded
+//        var modal = new bootstrap.Modal(document.getElementById('modalX'));
+//        modal.show();
+//    });
+//}
+
+function showDetails(url, id) {
+    $.ajax({
+        url: "/" + url + "/Details/" + id,
+        type: "GET",
+        processData: false,
+        contentType: false,
+        data: null,
+        async: true,
+        error: function (jqxhr, textStatus, errorThrown) {
+            console.error('Error', jqxhr);
+            $('.modal').remove();
+            $('.modal-backdrop').remove();
+            bootbox.alert(jqxhr.responseText);
+        }
+    }).done((data) => {
+        $('#modalX').empty();
+        $('#modalX').append(data);
+        $('.modal').modal('show');
+    });
+}
+
+
+function showEdit(url, id) {
+    $.ajax({
+        url: "/" + url + "/Edit/" + id,
+        type: "GET",
+        processData: false,
+        contentType: false,
+        data: null,
+        async: true,
+        error: function (jqxhr, textStatus, errorThrown) {
+            console.error('Error', jqxhr);
+            $('.modal').remove();
+            $('.modal-backdrop').remove();
+            bootbox.alert(jqxhr.responseText);
+        }
+    }).done((data) => {
+        // Log the received data to see what's being returned
+        console.log("Response data:", data);
+
+        // Make sure the modal element exists
+        if (!$('#modalX').length) {
+            $('body').append('<div class="modal fade" id="modalX" tabindex="-1" aria-hidden="true"></div>');
+        }
+
+        // Clear and append the new content
+        $('#modalX').empty();
+        $('#modalX').html(data);
+
+        // Initialize and show the modal after content is loaded
+        var modal = new bootstrap.Modal(document.getElementById('modalX'));
+        modal.show();
+    });
+}
+
+
+function confirmDelete(url, id) {
+    $.ajax({
+        url: "/" + url + "/Delete/" + id,
+        type: "GET",
+        processData: false,
+        contentType: false,
+        data: null,
+        async: true,
+        error: function (jqxhr, textStatus, errorThrown) {
+            console.error('Error', jqxhr);
+            $('.modal').remove();
+            $('.modal-backdrop').remove();
+            bootbox.alert(jqxhr.responseText);
+        }
+    }).done((data) => {
+        // Убедимся, что модальное окно существует
+        if (!$('#modalX').length) {
+            $('body').append('<div class="modal fade" id="modalX" tabindex="-1" aria-hidden="true"></div>');
+        }
+
+        // Очищаем и добавляем новое содержимое
+        $('#modalX').empty();
+        $('#modalX').html(data);
+
+        // Инициализируем и показываем модальное окно
+        var modal = new bootstrap.Modal(document.getElementById('modalX'));
+        modal.show();
     });
 }
